9ac636a57fceebaf7abd92fce967223e9b5350c6
Vastagon <jacob.yancey29@yahoo.com>
2022-11-02 18:35:04 -0500

    Added movement to pieces now. When clicking a valid move it will create a piece in that position, now I need to make it remove the piece as well, but I am tired lol

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,27 +1,25 @@
 import "../styles/board.css"
 import { useEffect, useState } from "react"
 import Piece from "./Piece"
-import boardPositions from "./boardpositions.json"
+import boardpositions from "./boardpositions.json"
 import {v4 as uuid} from "uuid"
 
 
 
 
 const Board = () =>{
-    let boardpositions
-    useEffect(() =>{
-        boardpositions = boardPositions
-    }, [])
+    const [boardPositions, setBoardPosition] = useState(boardpositions)
+    const [potentialMovement, setPotentialMovement] = useState([])
+    const [lastClickedPosition, setLastClickedPosition] = useState()
+
     const [boardDiv, setBoardDiv] = useState()
     let evenRow = true
-
 
     const [pieceClicked, setPieceClicked] = useState(false)
 
     ///Shows squares it can move
-    function canMove(potentialMovement){
-
-        setBoardDiv(boardpositions.map((prev, rowIndex) =>{
+    function canMove(){
+        setBoardDiv(boardPositions.map((prev, rowIndex) =>{
             evenRow = !evenRow
             return(<div key={uuid()} className="board-row">{prev.map((prev, index) =>{
                 evenRow = !evenRow
@@ -32,38 +30,41 @@ const Board = () =>{
                 for(let i = 0; i < potentialMovement?.length; i++){
                     if(JSON.stringify([rowIndex, index]) == JSON.stringify(potentialMovement[i])){
                         // hasDot = "has-dot " + hasDot
-                        hasDot = true
+                        hasDot = true 
                     }
                 }
 
-
+                ///onClick={() => squareClicked([rowIndex,index])}
                 return (<div key={uuid()} className={evenRow ? "board-square white-square" : "board-square green-square"}>
-                    <div className={hasDot ? "has-dot" : null} />
-                    <Piece boardPositions={boardPositions} key={uuid()} canMove={canMove} setPieceClicked={setPieceClicked} position={[rowIndex,index]} boardpositions={boardpositions} piece={boardpositions[rowIndex][index]}/>
+                    <div onClick={() => potentialMovementGetsClicked([rowIndex,index])} className={hasDot ? "has-dot" : null} />
+                    <Piece setLastClickedPosition={setLastClickedPosition} potentialMovementGetsClicked={potentialMovementGetsClicked} potentialMovement={potentialMovement} setPotentialMovement={setPotentialMovement} boardPositions={boardPositions} key={uuid()} canMove={canMove} pieceClicked={pieceClicked} setPieceClicked={setPieceClicked} position={[rowIndex,index]} boardpositions={boardpositions} piece={boardpositions[rowIndex][index]}/>
                 </div>)
             })}</div>) 
-            
+             
 
         }))
     }
 
 
-    useEffect(() =>{
-        setBoardDiv(boardpositions.map((prev, rowIndex) =>{
-            evenRow = !evenRow
-            return(<div key={uuid()} className="board-row">{prev.map((prev, index) =>{
-                evenRow = !evenRow
+    ///Runs whenever new piece is selected
+    useEffect(() =>{ 
+        canMove()
+    }, [potentialMovement])
 
-                return (<div key={uuid()} className={evenRow? "board-square white-square" : "board-square green-square"}>
-                    <Piece boardPositions={boardPositions} key={uuid()} canMove={canMove} setPieceClicked={setPieceClicked} position={[rowIndex,index]} boardpositions={boardpositions} piece={boardpositions[rowIndex][index]}/>
-                </div>)
-            })}</div>) 
-            
 
-        }))
-    }, [])
+    function potentialMovementGetsClicked(clickedSquare){
+        console.log(clickedSquare)
+        console.log(potentialMovement)
+
+        ///Checks if clicked square is in potentialMovement array
+        for(let i = 0; i < potentialMovement.length; i++){
+            console.log(JSON.stringify(potentialMovement[i]))
+            if(JSON.stringify(potentialMovement[i]) == JSON.stringify(clickedSquare)){
+                boardPositions[clickedSquare[0]][clickedSquare[1]] = boardPositions[lastClickedPosition[0]][lastClickedPosition[1]]
+            }
+        }
+    }
     
-
     if(!boardDiv) return null
 
     return(
--- src/components/Piece.js
+++ src/components/Piece.js
@@ -2,9 +2,6 @@ import "../styles/board.css"
 import boardPosition from "./boardpositions.json"
 
 const Piece = (props) =>{
-    ///Array of all positions that a piece can move
-    let potentialMovement
-    ///for pawns and kings
     let hasMoved = false
 
     ///easy check for color
@@ -13,75 +10,75 @@ const Piece = (props) =>{
         isWhite = false
     }
 
+    
     function clickPiece(){
-        console.log(props.position)
+        props.setLastClickedPosition(props.position)
+        console.log("Clicked piece position: " + props.position)
         ///PAWN LOGIC
         props.setPieceClicked(true) 
         if(props.piece === "whitePawn"){
             ///increase row by one, or two if first move
             if(!hasMoved && boardPosition[props.position[0]-1][props.position[1]] === ""){/// && boardPosition[props.position[0]-2][props.position[1]] === ""
-                potentialMovement = [[props.position[0]-2, props.position[1]], [props.position[0]-1, props.position[1]]]
+                props.setPotentialMovement([[props.position[0]-2, props.position[1]], [props.position[0]-1, props.position[1]]])
             }else{
                 if(boardPosition[props.position[0]-1][props.position[1]] === ""){
-                    potentialMovement = [[props.position[0]-1, props.position[1]]]
+                    props.setPotentialMovement([[props.position[0]-1, props.position[1]]])
                 }
             }
             ///Checking if pawn can take piece diagonally
             if((props.boardpositions[props.position[0]-1][props.position[1]-1]?.substring(0,5)) === "black"){
-                potentialMovement.push([props.position[0]-1, props.position[1]-1])
+                props.setPotentialMovement(prev => [...prev, ([props.position[0]-1, props.position[1]-1])])
             }
             if((props.boardpositions[props.position[0]-1][props.position[1]+1]?.substring(0,5)) === "black"){
-                potentialMovement.push([props.position[0]-1, props.position[1]+1])
+                props.setPotentialMovement(prev => [...prev, ([props.position[0]-1, props.position[1]+1])])
             }
         }
-        if(props.piece === "blackPawn"){
+        if(props.piece === "blackPawn"){///HERE NOW
             ///increase row by one, or two if first move
             if(!hasMoved && boardPosition[props.position[0]+1][props.position[1]] === ""){
-                potentialMovement = [[props.position[0]+2, props.position[1]], [props.position[0]+1, props.position[1]]]
+                props.setPotentialMovement([[props.position[0]+2, props.position[1]], [props.position[0]+1, props.position[1]]])
             }else{
                 if(boardPosition[props.position[0]+1][props.position[1]] === ""){
-                    potentialMovement = [[props.position[0]+1, props.position[1]]]
+                    props.setPotentialMovement([[props.position[0]+1, props.position[1]]])
                 }
             }
             ///Checking if pawn can take piece diagonally
             if((props.boardpositions[props.position[0]+1][props.position[1]+1]?.substring(0,5)) === "white"){
-                potentialMovement.push([props.position[0]+1, props.position[1]+1])
+                props.setPotentialMovement(prev => [...prev, [props.position[0]+1, props.position[1]+1]])
             }
             if((props.boardpositions[props.position[0]+1][props.position[1]-1]?.substring(0,5)) === "white"){
-                potentialMovement.push([props.position[0]+1, props.position[1]-1])
+                props.setPotentialMovement(prev => [...prev, [props.position[0]+1, props.position[1]-1]])
             }
         }
 
         ///Runs function on Board that shows potential movement
-        props.canMove(potentialMovement)
+        // props.canMove(props.potentialMovement)
 
         ///ROOK LOGIC
 
-
-        ///Will check if piece can move using potential movement 2d array
-        // movePiece(potentialMovement)
     }
-
-    function movePiece(potentialMovement){
-        // console.log(potentialMovement)
-        // console.log(props.position)
-    }
-    
-
 
 
     ///rendering
     if(props.piece !== undefined){
+        ///Displays piece
         return(
-        <div onClick={clickPiece} className="piece">
-            {props.piece ? <img className={props.piece !== undefined ? "chess-piece" : "piece"} src={props.color === "white" ? require(`../images/${props.piece}.png`) : require(`../images/${props.piece}.png`)} alt="Not here" />:
-            null}
-        </div>
+            <div onClick={clickPiece} className="piece">
+                {props.piece ? <img className={props.piece !== undefined ? "chess-piece" : "piece"} 
+                src={props.color === "white" ? require(`../images/${props.piece}.png`) : require(`../images/${props.piece}.png`)} 
+                alt="Not here" 
+            />:
+                null}
+            </div>
         )
     }else{
         return(
+            ///Displays empty square
             <div className="piece">
-                {props.piece ? <img className={props.piece !== undefined ? "chess-piece" : "piece"} src={props.color === "white" ? require(`../images/${props.piece}.png`) : require(`../images/${props.piece}.png`)} alt="Not here" />:
+                {props.piece ? <img className={props.piece !== undefined ? "chess-piece" : "piece"} 
+                src={props.color === "white" ? require(`../images/${props.piece}.png`) : require(`../images/${props.piece}.png`)} 
+                alt="Not here" 
+            />:
                 null}
             </div>                
         )
